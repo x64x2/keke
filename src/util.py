81ff2f8f941bd4140e4b02f2af0a5f05934cbe36
somto <81354428+x64x2@users.noreply.github.com>
2024-12-22 11:35:46 +0000

    Delete src directory

--- src/util.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def normalize_data(data):
-        C = (dose / self.Vd) * (self.ka / (self.ka - self.ke)) * (np.exp(-self.ke * time) - np.exp(-self.ka * time))
-        return C
-    
